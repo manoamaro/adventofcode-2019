e3dfe0f79ad3624241f723bbe7f86d04dbc9ec8d
Manoel Amaro <manoel.al.amaro@gmail.com>
2019-12-04 00:04:04 +0100

    Finishes day1

---
+++ day1/main.clj
@@ -0,0 +1,24 @@
+
+
+(defn calculate-fuel [mass]
+    (int (- (Math/floor (double (/ mass 3))) 2))
+)
+
+(defn calculate-fuel-with-fuel [mass]
+    (loop [current-fuel (calculate-fuel mass) sum 0]
+        (if (<= current-fuel 0)
+            sum
+            (recur (calculate-fuel current-fuel) (+ sum current-fuel))
+        )
+    )
+)
+
+(defn main []
+    (def raw-input (clojure.string/split-lines (slurp "input")))
+    (def module-masses (map #(Integer/parseInt %) raw-input))
+    (def fuel-per-module (map calculate-fuel-with-fuel module-masses))
+    (def total-fuel (reduce + fuel-per-module))
+    (println total-fuel)
+)
+
+(main)
