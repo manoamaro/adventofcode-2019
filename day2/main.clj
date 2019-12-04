94b14aaf9b6f1d96df5af478530b683e2e6bac71
Manoel Amaro <manoel.al.amaro@gmail.com>
2019-12-04 08:39:52 +0100

    day 2 part 1

---
+++ day2/main.clj
@@ -0,0 +1,29 @@
+(require '[clojure.string :as str])
+
+(defn main []
+    (def raw-input (str/split (slurp "input") #","))
+    (def operations (map #(Integer/parseInt %) raw-input))
+    (def operations-size (count operations))
+    (loop [result operations
+           pos 0]
+        (def operation (nth result pos))
+        (if (or (= operation 99) (>= pos operations-size))
+            (println result)
+            (let [
+                idx1 (nth result (+ pos 1))
+                idx2 (nth result (+ pos 2))
+                idx3 (nth result (+ pos 3))
+                [left right] (split-at idx3 result)
+                v1 (nth result idx1)
+                v2 (nth result idx2)
+                res (if (= operation 1) (+ v1 v2) (* v1 v2))]
+                (recur
+                    (concat left [res] (rest right))
+                    (+ pos 4)
+                )
+            )
+        )
+    )
+)
+
+(main)
