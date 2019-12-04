b3e0c280162387020a369e6206f07f38f8341a5e
Manoel Amaro <manoel.al.amaro@gmail.com>
2019-12-04 09:49:15 +0100

    finish part2 day2

--- day2/main.clj
+++ day2/main.clj
@@ -1,5 +1,11 @@
 (require '[clojure.string :as str])
 
+(defn replace-at [coll i value]
+    (let [[left right] (split-at i coll)]
+        (concat left [value] (rest right))
+    )
+)
+
 (defn compute [operations]
     (let [operations-size (count operations)]
         (loop [result operations
@@ -11,12 +17,11 @@
                     idx1 (nth result (+ pos 1))
                     idx2 (nth result (+ pos 2))
                     idx3 (nth result (+ pos 3))
-                    [left right] (split-at idx3 result)
                     v1 (nth result idx1)
                     v2 (nth result idx2)
                     res (if (= operation 1) (+ v1 v2) (* v1 v2))]
                     (recur
-                        (concat left [res] (rest right))
+                        (replace-at result idx3 res)
                         (+ pos 4)
                     )
                 )
@@ -25,11 +30,26 @@
     )
 )
 
-(defn main []
+(defn part1 []
     (def raw-input (str/split (slurp "input") #","))
     (def operations (map #(Integer/parseInt %) raw-input))
-
-    (println (compute operations))
+    (def operations-modified (replace-at (replace-at operations 1 12) 2 2))
+    (nth (compute operations-modified) 0)
 )
 
-(main)
+(defn part2 []
+    (def raw-input (str/split (slurp "input") #","))
+    (def operations (map #(Integer/parseInt %) raw-input))
+    (for [x (range 0 99)
+          y (range 0 99)
+          :let [
+            operations-modified (replace-at (replace-at operations 1 x) 2 y)
+            computed (compute operations-modified)
+            result (nth computed 0)
+          ]
+          :when (= result 19690720)] (+ (* 100 x) y)
+    )
+)
+
+(println (part1))
+(println (part2))
