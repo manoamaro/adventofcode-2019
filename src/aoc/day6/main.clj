dfcfa18722243188ea6d83257d0ca4f9d0fb97f3
Manoel Amaro <manoel@journiapp.com>
2020-02-16 14:50:20 +0100

    day 6

--- src/aoc/day6/main.clj
+++ src/aoc/day6/main.clj
@@ -2,22 +2,35 @@
   (:require [aoc.utils :refer :all]
             [clojure.string :as str]))
 
-(def map-test (mapv (fn [i] (str/split i #"\)")) ["COM)B" "B)C" "C)D" "D)E" "E)F" "B)G" "G)H" "D)I" "E)J" "J)K" "K)L"] ))
-(def orbits-map
+(def orbits-test1 (mapv (fn [i] (str/split i #"\)")) ["COM)B" "B)C" "C)D" "D)E" "E)F" "B)G" "G)H" "D)I" "E)J" "J)K" "K)L"] ))
+(def orbits-test2 (mapv (fn [i] (str/split i #"\)")) ["COM)B" "B)C" "C)D" "D)E" "E)F" "B)G" "G)H" "D)I" "E)J" "J)K" "K)L" "K)YOU" "I)SAN"] ))
+
+(def orbits-input
   (mapv
     (fn [i](str/split i #"\)"))
     (-> "src/aoc/day6/input.txt" slurp (str/split #"\n"))))
-(def map-objects (distinct (flatten orbits-map)))
 
-(defn count-orbits [map start]
-  (loop [count 0 pos start]
-    (if (= pos "COM")
-    count
-    (recur (inc count) (first (first (filter (fn [i] (= (last i) pos)) map)))))))
+(defn group-orbits
+  [orbits]
+  (reduce (fn [dict [parent child]] (assoc dict child parent)) {} orbits))
 
+
+(defn orbits-to-COM [orbits planet]
+  (take-while #(not= "COM" %) (iterate orbits planet)))
+
+(defn part1 []
+  (let [orbits (group-orbits orbits-input)
+        planets (keys orbits)]
+        (reduce + (map (fn [i] (count (orbits-to-COM orbits i) )) (keys orbits)))))
+
+(defn part2 []
+  (let [orbits (group-orbits orbits-input)
+        path-you (orbits-to-COM orbits "YOU")
+        path-san (orbits-to-COM orbits "SAN")
+        paths (concat path-you path-san)
+        single-paths (filter #(= 1 (second %)) (frequencies paths))]
+    (- (count single-paths) 2)))
+        
 (defn -main [& args]
-  (println (reduce
-    +
-    (map
-      (fn [i] (count-orbits orbits-map i))
-      map-objects))))
+  (println (part1))
+  (println (part2)))
